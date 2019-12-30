f8a6781d79f07052ae605aaee531075fbee5583f
chenbin5 <42439726+chenbin5@users.noreply.github.com>
2019-12-30 20:53:22 +0800

    get/set方法

--- src/main/java/com/immoc/sell/dataobject/ProductInfo.java
+++ src/main/java/com/immoc/sell/dataobject/ProductInfo.java
@@ -34,6 +34,83 @@ public class ProductInfo {
     private Date updateTime;
 
 
+    public String getProductId() {
+        return productId;
+    }
 
+    public String getProductName() {
+        return productName;
+    }
 
+    public BigDecimal getProductPrice() {
+        return productPrice;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public String getProductIcon() {
+        return productIcon;
+    }
+
+    public Integer getCategroyType() {
+        return categroyType;
+    }
+
+    public Integer getProductStock() {
+        return productStock;
+    }
+
+    public Integer getProductStatus() {
+        return productStatus;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductPrice(BigDecimal productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public void setProductIcon(String productIcon) {
+        this.productIcon = productIcon;
+    }
+
+    public void setCategroyType(Integer categroyType) {
+        this.categroyType = categroyType;
+    }
+
+    public void setProductStock(Integer productStock) {
+        this.productStock = productStock;
+    }
+
+    public void setProductStatus(Integer productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
 }
